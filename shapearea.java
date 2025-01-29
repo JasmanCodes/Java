a49dfc119bee9fd84dcb263ad0268853e0bb03cc
JasmanCodes <jasman120105@gmail.com>
2025-01-29 16:18:22 +0530

    Update shapearea.java

--- shapearea.java
+++ shapearea.java
@@ -1,21 +1,17 @@
 public class ShapeArea {
     
-    // Area of Rectangle
     public static double area(double length, double width) {
         return length * width;
     }
     
-    // Area of Square
     public static double area(double side) {
         return side * side;
     }
     
-    // Area of Circle
     public static double area(float radius) {
         return 3.14159 * radius * radius;
     }
     
-    // Area of Triangle
     public static double area(double base, double height, boolean isTriangle) {
         return 0.5 * base * height;
     }
